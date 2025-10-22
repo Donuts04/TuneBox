5a4b2045ca285f618123058733cc7ccec080a3a6
Osama Khalil <osamaqadoumi12@gmail.com>
2025-10-22 15:03:47 +0300

    fix asset prefix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  assetPrefix: process.env.NODE_ENV === "production" ? "/projects/tunebox" : "",
   images: {
     domains: [
       "i1.sndcdn.com",
